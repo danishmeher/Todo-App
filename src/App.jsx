83e0b4c59014c3dc554e32f4f2ac6dfba8942eae
Danish Riaz <Danishmeher086@gmail.com>
2025-09-06 22:25:13 +0500

    Fixed App.css import path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/navbar.jsx'
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import "./app.css"
+import "./App.css"
 
 
 
